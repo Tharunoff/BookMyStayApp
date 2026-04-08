13c4531947b474540e7fdb83f1f743f1a0ecedf1
Tharunoff <tharunoff@github.com>
2026-04-08 08:54:08 +0000

    Add feature/UC1-WelcomePage content

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,38 @@
+/**
+ * ---------------------------------------------------------------
+ * MAIN CLASS - UseCase1HotelBookingApp
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Hotel Booking Management System.
+ *
+ * At this stage, the application:
+ * - Starts execution from the main() method
+ * - Displays a welcome message to the user
+ * - Confirms that the system has started successfully
+ *
+ * No business logic, data structures, or user input
+ * is implemented in this use case.
+ *
+ * @author krishna
+ * @version 1.0
+ * ---------------------------------------------------------------
+ **/
+
+public class BookMyStayApp {
+
+    /**
+     * Application entry point.
+     * This method is executed when the program starts.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+
+    }
+}
